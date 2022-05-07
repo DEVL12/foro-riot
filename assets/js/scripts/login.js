9aed703896cb8c05a6460940cb35b0c0cdde950f
DEVL12 <Josediaz17822@gmail.com>
2022-05-07 16:53:11 -0400

    validacion del login LISTA

--- assets/js/scripts/login.js
+++ assets/js/scripts/login.js
@@ -1,26 +1,50 @@
 import validations from "./validations.js";
 const validar = new validations();
-//const formSearch = document.getElementById('IdDelFormulario'); // seleccionar el formulario segun un id
-//const inputs = document.querySelectorAll('#IdDelFormulario input');// seleccionar TODOS los inputs del formulario
+const formLogin = document.getElementById('formLogin');
+const inputs = document.querySelectorAll('#formLogin input');
 
-/*
-  let input = {
+let input = {
+  password : false,
+  username : false,
+}
 
+const comprobar = (e) => {
+  switch(e.target.name) {
+    case 'username': validarInput(e.target, e.target.name); break;
+    case 'password': validarInput(e.target, e.target.name); break;
   }
+}
 
-  const comprobar = (e) => {
+inputs.forEach((inputs) => {
+   inputs.addEventListener('keyup', comprobar);
+});
 
+const validarInput = (target, name) => {
+  if((validar.ValidateText(target.value, true, true, true, " _\\-¡!¿?:", "4,40"))) {
+    input[name] = true;
+    document.getElementById(name).classList.add('validado-input');
+    document.getElementById(name).classList.remove('errores-input');
+  } else {
+    input[name] = false;
+    document.getElementById(name).classList.remove('validado-input');
+    document.getElementById(name).classList.add('errores-input');
   }
+}
 
-  inputs.forEach((inputs) => {
-
-  });
-
-  const validarInput = (target, name) => {
-
+formLogin.addEventListener('submit', e => {
+  e.preventDefault();
+  if(input.password && input.username) {
+    formLogin.reset();
+  } else {
+    document.querySelector('.log_reg_table_msg').innerHTML =
+    '<div class="error">'+
+      '<p><em>ERROR AL INICIAR SESIÓN</em></p>'+
+        '<ul style = "color: crimson;">'+
+          '<li>La contraseña o el nombre de usuario son incorrectos</li>'+
+          '<li>El nombre de usuario y la contraseña debe tener un minimo de 4 y un maximo de 40 caracteres</li>'+
+          '<li>No se aceptan simbolos extraños</li>'+
+        '</ul>'+
+    '</div>'+
+    '<br>';
   }
-
-  formSearch.addEventListener('submit', e => {
-
-  });
-*/
+});
--- views/session/login.php
+++ views/session/login.php
@@ -10,45 +10,34 @@
 						<img src="<?= base_url() ?>assets/images/darko-logo-white.png" style="height:35px;width:auto;">
 					</a>
 					<br><br>
-					<div id="log_reg_table">
-						<div class="error">
-							<p><em>Corrige los siguientes errores antes de continuar:</em></p>
-								<ul>
-									<li>
-										Mensaje....
-										HOLA EL CAMPO DEBE TENER TAL..
-										Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque, iste!
-									</li>
-								</ul>
-						</div>
-						<br>
+					<div id ="log_reg_table" class="log_reg_table_msg">
+
 					</div>
+					<br>
 				</center>
-				<form>
+				<form id = "formLogin">
 					<table border="0" cellspacing="0" cellpadding="5" class="tborder" id="log_reg_table" style="padding:30px;padding-bottom:10px;">
 						<tr>
 							<td class="trow1" style="border:none;">
-								<div class="smalltext login_text_titles">Username</div>
-								<input type="text" class="textbox" name="username" size="25" style="width: 100%;" value="" />
+								<div class="smalltext login_text_titles">Nombre de usuario</div>
+								<input type="text" class="textbox" name="username" id = "username" size="25" style="width: 100%;" value="" />
 							</td>
 						</tr>
 						<tr>
 							<td class="trow2">
-								<div class="smalltext login_text_titles">Password</div>
-								<input type="password" class="textbox" name="password" size="25" style="width: 100%;" value="" />
-								<br><a href="<?= base_url() ?>">¿Perdiste tu contraseña?</a>
+								<div class="smalltext login_text_titles">Contraseña</div>
+								<input type="password" class="textbox" name="password" id = "password" size="25" style="width: 100%;" value="" />
 							</td>
 						</tr>
 						<tr>
-							<td class="trow1" colspan="2" align="center"><label title="Si lo marcas, tu inicio de sesión se recordará en este ordenador, en otro caso, se finalizará la sesión al cerrar el navegador."><input type="checkbox" class="checkbox" name="remember" checked="checked" value="yes" /> Recordarme</label>
-								<br><br>
+							<td class="trow1" colspan="2" align="center">
 								<div align="center"><input type="submit" class="button log_reg_btn" name="submit" value="Iniciar sesión" />
 								</div>
 							</td>
 						</tr>
 					</table>
 				</form>
-				<br>
+				<br><br>
 				<center>
 					<div class="smalltext white_text">¿No tienes una cuenta? <a href="<?= base_url() ?>session/register" class="white_text" style="font-weight:600;">¡Registrate!</a>
 
