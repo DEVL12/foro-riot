f54b20aba0bc522e751d1ad5b57ceea20f6a0568
Atlantox7 <49962482+Atlantox@users.noreply.github.com>
2022-05-09 12:33:29 -0400

    validación de la búsqueda + quitar ValidateSearch

--- assets/js/scripts/search.js
+++ assets/js/scripts/search.js
@@ -1,26 +1,49 @@
 import validations from "./validations.js";
 const validar = new validations();
-//const formSearch = document.getElementById('IdDelFormulario'); // seleccionar el formulario segun un id
-//const inputs = document.querySelectorAll('#IdDelFormulario input');// seleccionar TODOS los inputs del formulario
+const formSearch = document.getElementById('formSearch');
 
-/*
-  let input = {
+let allOK = true;
 
+const validateInput = (target, simbols, limit) => {
+  if((validar.ValidateText(target.value, true, true, true, simbols, limit))) {
+    ChangeClass("correct", target.name);
+  } else {
+    ChangeClass("error", target.name);
+  }
+}
+
+const ChangeClass = (condicion, name) => {
+  if(condicion === "correct") {
+    document.getElementById(name).classList.add('validado-input');
+    document.getElementById(name).classList.remove('errores-input');
+  } else if(condicion === "error") {
+    allOK = false;
+    document.getElementById(name).classList.remove('validado-input');
+    document.getElementById(name).classList.add('errores-input');
+  } else if(condicion == "alert") {
+    document.getElementById(name).classList.add('alert-input');
+  } else if(condicion == "cleanAlert") {
+    document.getElementById(name).classList.remove('alert-input');
+  }
+}
+
+formSearch.addEventListener('submit', e => {
+  e.preventDefault();
+  if(validar.CheckIfEmpty(formSearch.author.value)){
+    validateInput(formSearch.author, " _\\-¡!¿?:.^$", "3,16");
   }
 
-  const comprobar = (e) => {
-
+  if(validar.CheckIfEmpty(formSearch.keywords.value)){
+    validateInput(formSearch.keywords,"", "1,9999");
   }
 
-  inputs.forEach((inputs) => {
-
-  });
-
-  const validarInput = (target, name) => {
-
+  if(parseInt(formSearch.numreplies.value)< 0){
+    ChangeClass("error",formSearch.numreplies.name);
   }
-
-  formSearch.addEventListener('submit', e => {
-
-  });
-*/
+  
+  if(allOK){
+    alert("realizando búsqueda....");
+  }else{
+    alert("Hay campos inválidos");
+  }
+});
--- assets/js/scripts/validations.js
+++ assets/js/scripts/validations.js
@@ -28,65 +28,6 @@ class validations {
     expresion = new RegExp(expresion);
     return expresion.test(data);
   }
-
-  ValidateSearch(keywordSearch = '', keywordSearchType = '',
-                 forumToSearch = '', topicSearch = '',
-                 nicknameSearch = '', equalNickname = '',
-                 answerSerachType = '', answerSearch ='',
-                 messageSearchType ='', messageInterval = '',
-                 orderCriterion = '', orderType = '',
-                 showResult = '') {
-
-    var keywordSearchOK = this.CheckIfEmpty(keywordSearch);
-    //var keywordSearchTypeOK = CheckIfEmpty(keywordSearchType);
-    //var forumToSearchOK = CheckIfEmpty(forumToSearch);
-    //var topicSearchOK = CheckIfEmpty(topicSearch);
-
-    var nicknameSearchOK = this.CheckIfEmpty(nicknameSearch);
-    //var equalNicknameOK = CheckIfEmpty(equalNickname);
-
-    //var answerSerachTypeOK = CheckIfEmpty(answerSerachType);
-    var answerSearchOK = this.CheckIfEmpty(answerSearch);
-
-    //var messageSearchTypeOK = CheckIfEmpty(messageSearchType);
-    //var messageIntervalOK = CheckIfEmpty(messageInterval);
-
-    //var orderCriterionOK = CheckIfEmpty(orderCriterion);
-    //var orderTypeOK = CheckIfEmpty(orderType);
-
-    //var showResultOK = CheckIfEmpty(showResult);
-
-    /*
-      Si por alguna razón necesitas validar si los demás filtros están vacíos, aquí esta el if y arriba
-      los chequeos sobre si está vacío, yo lo hice sólo teniendo en cuenta los filtros que
-      no están puestos por defecto en la página, además agregué el filtro de tema (topicSearch), el cual corresponde al
-      tema de discusión, ejemplo: bug, queja, reclutamiento, etc.
-
-      if(!keywordSearchOK && !keywordSearchTypeOK &&
-          !forumToSearchOK &&
-          !topicSearchOK &&
-          !nicknameSearchOK && !equalNicknameOK &&
-          !answerSerachTypeOK && !answerSearchOK &&
-          !messageSearchTypeOK && !messageIntervalOK &&
-          !orderCriterionOK && !orderTypeOK &&
-          !showResultOK){
-              console.log('Los filtros están vacíos');
-      }
-    */
-    if(!keywordSearchOK && !nicknameSearchOK && !answerSearchOK){
-      console.log('Debes rellenar al menos un campo de búsqueda')
-    }
-    else
-    {
-      //se aplican los filtros de búsqueda yendo de lo general a lo más específico, ejemplo:
-      //se buscan primero por palabras clave
-      //de esos escoges los que cuyo nombre de invocador coincida
-      //de esos escoges los que tengan n cantidad de respuestas
-      //etc
-      //si en algún momento ya no hay resultados se detiene todo el procedimiento para no hacer trabajar innecesariamente
-      //el procesador aplicando los filtros siguientes
-    }
-  }
 }
 
 export default validations;
