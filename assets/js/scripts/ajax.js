74068ef2f2e05ba01b09bacdd8bef8b8738fc5a5
DEVL12 <Josediaz17822@gmail.com>
2022-06-16 12:21:10 -0400

    Creacion de la funcion sendGet en la clase ajax.js

--- assets/js/scripts/ajax.js
+++ assets/js/scripts/ajax.js
@@ -7,6 +7,14 @@ class Ajax{
     request.send(formData);
     return request;
   }
+
+  sendGet(url) {
+    var request = (window.XMLHttpRequest) ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    var ajaxUrl = base_url+url;
+    request.open("GET",ajaxUrl, true);
+    request.send();
+    return request;
+  }
 }
 
 export default Ajax;
