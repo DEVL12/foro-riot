0d811244333c521e9aa9569519fa69e562193809
Atlantox7 <49962482+Atlantox@users.noreply.github.com>
2022-06-20 16:21:06 -0400

    ahora se puede dar, quitar y modificar

--- controllers/answer.php
+++ controllers/answer.php
@@ -1,6 +1,7 @@
 <?php
   class answer extends controllers
   {
+
     public function __construct()
     {
       session_start();
@@ -10,12 +11,20 @@
     public function answers_for($id_discussion)
     {
       $answers = $this->model->GetAllAnswersOfADiscussion($id_discussion);
+      $honors = [[], []];
 
+      array_push($honors[0], $this->model->GetHonorsOf($id_discussion,'discusion'));
+
+
+      for($i = 0; $i < count($answers); $i++){
+        array_push($honors[1], $this->model->GetHonorsOf($answers[$i]['id_respuesta'],'respuesta'));
+      }
 
       $data = [
         'title' => "tal usuario - Respuestas",
         'script' => "answer.js",
-        'data_answers' => (!empty($answers)) ? $answers : '<h1 style="color: rgba(186, 51, 64, 1);">Lo sentimos, no hay respuestas registradas.</h1>'
+        'data_answers' => (!empty($answers)) ? $answers : '<h1 style="color: rgba(186, 51, 64, 1);">Lo sentimos, no hay respuestas registradas.</h1>',
+        'honors' => $honors
       ];
 
       $this->views->getViews($this,"answer",$data);
